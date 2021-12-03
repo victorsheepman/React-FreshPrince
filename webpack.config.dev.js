7d155973385e409bd18fc99d0b5cc66019a4a991
unknown <chirlychplin@gmail.com>
2021-12-03 14:19:32 -0400

    optimizacion 1.1

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,8 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -45,7 +47,8 @@ module.exports = {
     minimizer: [
       new CssMinimizerPlugin(),
       new TerserPlugin()
-    ]
+    ],
+
   },
   plugins: [
     new HtmlWebpackPlugin({
